c1ffe0b25f65c491dc47447a27fa90a8d96f9127
SofiaBachulashvili <148392385+SofiaBachulashvili@users.noreply.github.com>
2024-05-16 17:19:15 +0300

    Update ClassAdapterTest.cpp

--- tests/ClassAdapterTest.cpp
+++ tests/ClassAdapterTest.cpp
@@ -1,20 +1,11 @@
 #include <gtest/gtest.h>
 #include "../adapter/ClassAdapter.cpp"
 
-// Тест для класса Adapter
-TEST(AdapterTest, RequestTest) {
-  Adapter adapter;
-  
-  // Проверяем, что метод request делегирует вызов метода specificRequest объекту Adaptee
-  testing::internal::CaptureStdout();
-  adapter.request();
-  std::string output = testing::internal::GetCapturedStdout();
-  
-  EXPECT_EQ(output, "specific request\n");
-}
-
-TEST(AdapteeTest, SpecificRequestPrintsCorrectMessage) {
+// Тест для класса Adaptee
+TEST(AdapteeTest, SpecificRequestTest) {
   Adaptee adaptee;
+
+  // Проверяем, что метод specificRequest выводит правильное сообщение
   testing::internal::CaptureStdout();
   adaptee.specificRequest();
   std::string output = testing::internal::GetCapturedStdout();
@@ -22,15 +13,41 @@ TEST(AdapteeTest, SpecificRequestPrintsCorrectMessage) {
   EXPECT_EQ(output, "specific request\n");
 }
 
-TEST(AdapterTest, AdapterCreation) {
+// Тест для класса Adapter
+TEST(AdapterTest, RequestTest) {
   Adapter adapter;
 
-  // Поскольку класс Adapter наследует класс Adaptee приватно, нам нельзя напрямую вызвать метод specificRequest здесь.
-  // Для тестирования мы будем использовать макетный объект, чтобы проверить вызов метода.
-  MockAdapter mockAdapter;
-  EXPECT_CALL(mockAdapter, specificRequest());
-
+  // Проверяем, что метод request делегирует вызов метода specificRequest объекту Adaptee
+  testing::internal::CaptureStdout();
   adapter.request();
+  std::string output = testing::internal::GetCapturedStdout();
+
+  EXPECT_EQ(output, "specific request\n");
+}
+
+// Тест для проверки взаимодействия Target с Adapter
+TEST(TargetAndAdapterTest, RequestTest) {
+  class FakeTarget : public Target {
+  public:
+    void request() override {
+      std::cout << "fake request\n";
+    }
+  };
+  
+  FakeTarget fakeTarget;
+  Adapter adapter;
+
+  // Проверяем, что метод request вызывает метод request у Target
+  testing::internal::CaptureStdout();
+  fakeTarget.request();
+  std::string targetOutput = testing::internal::GetCapturedStdout();
+
+  testing::internal::CaptureStdout();
+  adapter.request();
+  std::string adapterOutput = testing::internal::GetCapturedStdout();
+
+  EXPECT_EQ(targetOutput, "fake request\n");
+  EXPECT_EQ(adapterOutput, "specific request\n");
 }
 
 int main(int argc, char **argv) {
